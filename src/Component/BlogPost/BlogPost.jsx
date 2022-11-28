ab6da4f1f7bedd1ab7dbd6e51b3804911e067ee6
pgatech <prayogahakimm@gmail.com>
2022-11-28 14:44:28 +0700

    Add JSON Dynamic

--- src/Component/BlogPost/BlogPost.jsx
+++ src/Component/BlogPost/BlogPost.jsx
@@ -3,11 +3,32 @@ import Post from "../Post/Post";
 import './BlogPost.css';
 
 class BlogPost extends Component {
+    state = {
+        post: []
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                post: json 
+            })
+        })
+    }
+
     render () {
         return (
             <Fragment>
                 <p className="section-title">Blog Post</p>
-                <Post title="title" desc="desc"/>
+                {
+                    this.state.post.map(post => {
+                        return (
+                            <Post key={post.id} title={post.title} desc={post.body}/>
+                        )
+                    })
+                }
+                
             </Fragment>
         )
     }
